0f9adb53e45cd0d903b8e7aab851ba2fa943e929
lyndoncortez <lyndoncortez@yahoo.com>
2021-01-18 19:37:27 +0800

    Update

--- Playing Cards/cards.js
+++ Playing Cards/cards.js
@@ -1,32 +1,90 @@
-const suits = ["♤", "♢", "♣", "♡"];
-const values = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"];
+// Card Properties
+const suits = ['♦','♥','♠','♣']
+const values = ['Two','Three','Four','Five','Six','Seven','Eight','Nine','Ten','Jack','Queen','King','Ace']
+const nValues = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
 
-let deck = [];
+// Card Object
+function Card(suit, value, nValue) {
+    this.suit = suit;
+    this.value = value;
+    this.nValue = nValue;
+    this.dealt = false; 
+    this.name = this.value + ' of ' + this.suit;
+}
 
-for (let i = 0; i < suits.length; i++) {
-    for (let x = 0; x < values.length; x++) {
-        let card = suits[i] + values[x] ;
-        deck.push(card);
-        
+// Build Deck
+function generateDeck() {
+    gameDeck = []
+    for (let i = 0; i < suits.length; i++) {
+        for (let j = 0; j < values.length; j++) {
+            let card = new Card(suits[i], values[j], nValues[j]);
+            gameDeck.push(card);
+        }
+    }
+    console.log(gameDeck);
+}
+
+// Shuffle Deck
+function shuffleDeck(gameDeck) {
+    gameDeck.sort(() => Math.random() - 0.5);
+    for (let i = 0; i < gameDeck.length; i++) {
+        gameDeck[i].dealt = false;
+    }
+    console.log(gameDeck);
+}
+
+// Arrange Deck by Suit
+function arrangeBySuit(gameDeck) {
+    gameDeck.sort((a, b) => {
+        let cardA = a.suit.toLowerCase();
+        let cardB = b.suit.toLowerCase();
+        if (cardA < cardB) {
+            return -1;
+        }
+        if (cardA > cardB) {
+            return 1;
+        }
+        return 0;
+    });
+    console.log(gameDeck);
+}
+
+// Arrange Deck by Value
+function arrangeByValue(gameDeck, mode = 'asc') {
+    if (mode === 'asc') {
+        gameDeck.sort((a, b) => {
+            return a.nValue - b.nValue;
+        });
+    } else {
+        gameDeck.sort((a, b) => {
+            return b.nValue - a.nValue;
+        });
+    }
+    console.log(gameDeck);
+}
+
+// Deal 1 Card
+function dealCard(gameDeck) {
+    let dCard = gameDeck[0];
+    gameDeck.splice(0, 1);
+    gameDeck.splice(gameDeck.length, 0, dCard);
+    if (dCard.dealt !== true) {
+        dCard.dealt = true;
+        return dCard;
+    } else {
+        console.log('No more cards');
     }
 }
 
-/*  console.log(deck); */
-
-//Shuffle cards
-function shuffleCards(arr) {
-		for (let i = arr.length - 1; i > 0; i--) {
-		let j =  Math.floor(Math.random() * i);
-  	let temp = arr[i];
-  	arr[i] = arr[j];
-  	arr[j] = temp;
-	}
-  return console.log(arr);
+// Deal 5 Cards or 1 Hand
+function dealHand(gameDeck) {
+    dealtHand = [];
+    for (let i = 0; i < 5; i++) {
+        dealtHand[i] = (dealCard(gameDeck));
+        if (dealtHand[i] === undefined) {
+            return;
+        }
+    }
+    console.log(dealtHand);
+    handValue(dealtHand);
 }
-
-/* shuffleCards(deck); */
-
-function suitCards(arr){
-	
-}
-
