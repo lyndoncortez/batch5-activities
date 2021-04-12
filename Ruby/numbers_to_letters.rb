e56b29f7a98651abc7e13bfbf242e0ce5aaca522
lyndoncortez <lyndoncortez@yahoo.com>
2021-04-12 18:36:53 +0800

    Added test spec

--- Ruby/numbers_to_letters.rb
+++ Ruby/numbers_to_letters.rb
@@ -1,16 +1,16 @@
 class NumbersToLetters
-  Alpha26 = ("A".."Z").to_a
+  ALPHA26 = ("A".."Z").to_a
   def int_to_char(num)
     return "" if num < 1
     a, b = "", num
     loop do
       b, c = (b - 1).divmod(26)   # b = quotient, c = remainder
-      a.prepend(Alpha26[c])       # a = "c"
+      a.prepend(ALPHA26[c])
       break if b.zero?
     end
     a
   end
 end
 
-# convert = NumbersToLetters.new
-# puts convert.int_to_char(100)
+# con = NumbersToLetters.new
+# puts con.int_to_char(100)
