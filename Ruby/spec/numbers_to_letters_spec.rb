3fb870eb84e165135b132a41b7abbf2e4e63e751
lyndoncortez <lyndoncortez@yahoo.com>
2021-04-12 20:20:50 +0800

    update test spec

--- Ruby/spec/numbers_to_letters_spec.rb
+++ Ruby/spec/numbers_to_letters_spec.rb
@@ -1,8 +1,12 @@
 require './numbers_to_letters'
 
-describe 'numbers_to_letters' do
-  it 'converts integer to character' do
-    c = NumbersToLetters.new
-    expect(c.int_to_char(1000)).to eq('ALL')
+RSpec.describe NumbersToLetters do
+  c = NumbersToLetters.new
+  num = 1000
+
+  context "convert method is called" do
+    it 'converts integer to character' do
+      expect(c.int_to_char(num)).to eq('ALL')
+    end
   end
 end
